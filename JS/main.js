ce52a16bab79bd245e079aab93c70e50631891ae
Migueldl1002 <migueld1002@gmail.com>
2024-04-03 11:42:43 -0500

    Alerta JS

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+let listElements = document.querySelectorAll('.List_button-Click');
+listElements.forEach(listelement => {
+    listelement.addEventListener('click', ()=>{
+        alert()
+    }) 
+}); 
